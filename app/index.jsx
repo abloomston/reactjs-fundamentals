46ab8cceb761c2436c8e3fc50a091bcd855f06b1
Adam Bloomston <mail@adambloomston>
2016-03-08 09:41:20 -0500

    playing with props and child component

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ColorList = React.createClass({
+  render: function(){
+    return (
+      <ul>
+        {this.props.colors.map(color => <li>{color}</li>)}
+      </ul>
+    );
+  }
+});
 var HelloWorld = React.createClass({
   render: function(){
     return (
       <div>
-        Hello World!
+        <h3>Hello, {this.props.name}</h3>
+        <h4>You like the following colors:</h4>
+        <ColorList colors={this.props.colors}/>
       </div>
     );
   }
 });
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(<HelloWorld name="Adam" colors={["orange","blue","black"]} />, document.getElementById('app'));
