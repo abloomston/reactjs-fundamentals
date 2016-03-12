43eedd8d8c4d176df54633fd5ecbbd2aab14faae
Adam Bloomston <mail@adambloomston>
2016-03-12 16:00:18 -0500

    home cleanup
    * functional stateless component
    * proptypes

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,17 +1,20 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
-var transparentBg = require('../styles/index.jsx').transparentBg;
+var styles = require('../styles/index.jsx');
 
-var Home = React.createClass({
-	render: function () {
-		return (
-			<div className='jumbotron col-sm-12 text-center' style={transparentBg}>
-				<h1>Github Battle</h1>
-				<p className="lead">Some fancy motto</p>
-        <button className="btn btn-lg btn-success" onClick={this.props.route.startOver}>Get Started</button>
-			</div>
-		);
-	}
-});
+var Home = props => (
+	<div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+		<h1>Github Battle</h1>
+		<p className="lead">Some fancy motto</p>
+    <button className="btn btn-lg btn-success" onClick={props.route.startOver}>Get Started</button>
+	</div>
+);
+
+Home.propTypes = {
+  route: PropTypes.shape({
+    startOver: PropTypes.func.isRequired
+  })
+};
 
 module.exports = Home;
