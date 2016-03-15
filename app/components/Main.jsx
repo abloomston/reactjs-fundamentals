3269c5e8fbc3cd65d46a68eca27647c7f7c9a30f
Adam Bloomston <mail@adambloomston>
2016-03-15 15:54:22 -0400

    css transitions from video 12

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,8 +1,15 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = props => (
 	<div className='main-container'>
-    {props.children}
+    <ReactCSSTransitionGroup
+            transitionName="appear"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+      {React.cloneElement(props.children, {key: props.location.pathname})}
+    </ReactCSSTransitionGroup>
   </div>
 );
 
